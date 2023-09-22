0a163e1478a79cff42dc43f8ed6dc87bc84b69ec
Santiago Salazar <kalaxdj@gmail.com>
2023-09-22 13:09:16 -0500

    Adding crate post button in post component and adding back to posts button in CreatePost component

--- src/components/PostCreate.jsx
+++ src/components/PostCreate.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function PostCreate() {
   const [formData, setFormData] = useState({
@@ -21,6 +22,31 @@ function PostCreate() {
 
   return (
     <>
+      <div>
+        <Link to="/posts"
+          style={{
+            backgroundColor: '#3498db',
+            border: '2px solid #2980b9',
+            color: 'white',
+            padding: '10px 20px',
+            borderRadius: '5px',
+            fontSize: '16px',
+            cursor: 'pointer',
+            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+            transition: 'background-color 0.3s ease',
+            textDecoration: 'none',
+          }}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{
+            width : '15px',
+            height : 'auto',
+            marginRight : '5px'
+          }}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+          </svg>
+
+          Back to posts
+        </Link>
+      </div>
       <div className="card">
         <div className="card-body">
           <h2 className="card-title">Create a New Post</h2>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,6 +4,7 @@
 import MUIDataTable from "mui-datatables";
 import { useContext } from "react";
 import { PostContext } from "../Contexts/PostContext";
+import { Link } from "react-router-dom";
 
 //MUIDataTable config
 const MUI_OPTIONS = {
@@ -15,8 +16,27 @@ function Posts() {
 
     return (
         <>
-            <header>
+            <header style={{
+                display : 'flex',
+                alignItems : 'center',
+                justifyContent : 'space-between'
+            }}>
                 <h1>Posts</h1>
+                <Link to="/create"
+                style={{
+                    backgroundColor: '#3498db', 
+                    border: '2px solid #2980b9', 
+                    color: 'white', 
+                    padding: '10px 20px', 
+                    borderRadius: '5px', 
+                    fontSize: '16px', 
+                    cursor: 'pointer', 
+                    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', 
+                    transition: 'background-color 0.3s ease',
+                    textDecoration: 'none'
+                }}>
+                    Create post
+                </Link>
             </header>
             <hr />
             <main className="dataTable_container">
