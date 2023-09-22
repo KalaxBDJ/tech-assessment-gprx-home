46531bff11375f96ba7e260bf3efdf5b0fc996a6
Santiago Salazar <kalaxdj@gmail.com>
2023-09-22 13:11:18 -0500

    Adding back to posts button styles

--- src/components/PostCreate.jsx
+++ src/components/PostCreate.jsx
@@ -37,9 +37,9 @@ function PostCreate() {
             textDecoration: 'none',
           }}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{
-            width : '15px',
-            height : 'auto',
-            marginRight : '5px'
+            width: '15px',
+            height: 'auto',
+            marginRight: '5px'
           }}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
           </svg>
